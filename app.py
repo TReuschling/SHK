c31ceadb28d3295da78d301434b58c316107f961
TReuschling <77385529+TReuschling@users.noreply.github.com>
2021-08-25 14:53:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if nav == "Server":
     Spalte 1 (Colab, Binder...) ist nicht relevant, da es sich um ein JupyterNotebook geht.  
       
     '''
-    image = Image.open("/Users/tassiloreuschling/Uni/SHK_Stelle/Code/Bilder/Aufstellung1.png")
+    image = Image.open("https://github.com/TReuschling/SHK/blob/main/Aufstellung1.png")
     st.image(image, caption = "Aufstellung")
     '''
     __Preiskalkulation für Littlest Jupyterhub:__  
